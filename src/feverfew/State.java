17f015056f8cabddfc07e5a919786959675e0f7c
Oscar <Oscar@Enkidu.lan>
2014-01-27 18:10:10 +0000

    Enum-type class for tracking the internal state of the game

---
+++ src/feverfew/State.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package feverfew;
+
+/**
+ *
+ * @author Oscar
+ * 
+ * Represents the internal state/stage of the game, allowing for cycle() to
+ * know what to do in any given state without a mess of if statements
+ * 
+ * Not a subclass of board since FLogger also needs to access it, and adding
+ * a public inner class to board just makes it even more cluttered and overused.
+ * 
+ * No need for states to have any internal structure or values
+ */
+public enum State {
+    STARTINGCOMBAT, CREATINGCOMBAT, RUNNINGCOMBAT, WAITINGINPUT
+}
