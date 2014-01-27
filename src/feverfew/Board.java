6d8a9703a185342629a642587095897d689baf4f
Oscar <Oscar@Enkidu.lan>
2014-01-27 18:06:44 +0000

    Added State to track current phase of game A Combat is now created and creates Rounds appropriately

--- src/feverfew/Board.java
+++ src/feverfew/Board.java
@@ -16,14 +16,20 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Shape;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.font.GlyphVector;
 import java.awt.geom.AffineTransform;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.EventListener;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JButton;
 
 import javax.swing.JPanel;
 
@@ -32,46 +38,51 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 
 
-public class Board extends JPanel implements Runnable, Testing {
+public class Board extends JPanel implements Runnable, Testing, ActionListener {
     // right
+    ClassLoader cl;
     
     private Thread animator;
     private final static int DELAY = 30;
     
+    // This enum tracks the game's internal state, allowing control over
+    // what is happening
+    private State state;
+    
     // The player character, currently singular
     private PlayerCharacter player;
     
-    private ArrayList<Pin> screenPins;
-    private int pinCount;
     // keep track of objects to be painted
     // iteration over this array will paint each object without needing many 
     // lines of painting code
+    private ArrayList<Pin> screenPins;
+    private int pinCount;
     
     //graphics context
     private Graphics2D g2d;    
     
     // Combat object. When this exists, a battle is taking place.
     private Combat currentFight;
-    
-    ClassLoader cl; 
-    // Will be needed for loading images & stuff
+    // Round, a single round of combat
+    private Round round;     
     
     private boolean gameActive;
-    private Round round = null;    
+    private boolean userReady;
     
     // Extractors for XML docs
     private ActionExtractor actionGetter;
     private EnemyExtractor enemyGetter;
     
+    public KeyAdapter myAdapter;
+    public ActionListener myListener;
+    
     public Board(ClassLoader loader){
-        //TODO add inputs? Hopefully never needed
-        
+               
         // Setting some JPanel constants relevant to the desired setup
         this.setBackground(BACKGROUND);
         this.setDoubleBuffered(true);
         this.setPreferredSize(DIM);
-        this.setFocusable(true);
-        
+        this.setFocusable(true);        
         
         cl = loader;
         
@@ -84,7 +95,9 @@ public class Board extends JPanel implements Runnable, Testing {
     
     private void gameStart(){
         // Initialises the game
-        this.addKeyListener(new MyAdapter());
+        myAdapter = new MyKeyAdapter();
+        this.addKeyListener((KeyListener) myAdapter);
+//        myListener = new MyActionAdapter();
         
         LOGGER.log("Extracting XML...");
         
@@ -97,14 +110,18 @@ public class Board extends JPanel implements Runnable, Testing {
             LOGGER.log("XML extraction failed.");
             ex.printStackTrace();
             System.exit(1);
-        }
+        }        
+        
         
         LOGGER.log("Initialising player character");
         player = new PlayerCharacter(actionGetter.getListOfActions());
         
-        //actbar = new ActBar(ACTBAR_DURATION, ACTBAR_STOPCOUNT, 
-                //ACTBAR_DIFFICULTIES);
+        // Set the state and log the change
+        state = State.STARTINGCOMBAT;
+        LOGGER.stateSet(state);
+        
         gameActive = true;
+        userReady = false;
         
         screenPins = new ArrayList(MAXSCREENOBJECTS);
         pinCount = 0;
@@ -118,6 +135,14 @@ public class Board extends JPanel implements Runnable, Testing {
         animator.start();
     }
     
+    
+    /**
+     * <p>
+     * Painter method for Feverfew. Cycles over objects in the screenPins list.
+     * </p>
+     * @param g Graphics object that the painter will use
+     * 
+     */
     @Override
     public void paint(Graphics g) {
         super.paint(g);
@@ -169,11 +194,8 @@ public class Board extends JPanel implements Runnable, Testing {
                         g2d.draw(trans.createTransformedShape(
                                 paintable.getShape()));
                     }
-                    
                 }
-                
             }
-            
         }
         
         Toolkit.getDefaultToolkit().sync();
@@ -190,10 +212,50 @@ public class Board extends JPanel implements Runnable, Testing {
              * 3) Then cycle over the combat until it's done
              * 4) goto 1
              */
+            
+            switch (state){
+                case STARTINGCOMBAT:
+                    // Code to create a button
+                    JButton combatStartButton = new JButton("Let's go!");
+                    // The command that the button sends when used
+                    combatStartButton.setActionCommand("startCombat");
+                    // The switchboard that listens for said command
+                    combatStartButton.addActionListener(this);
+                    // Place appropriately, or, try to
+                    combatStartButton.setLocation(COMBATSTARTBUTTONLOCATION);
+                    
+                    // Add the button to the board
+                    this.add(combatStartButton);
+                    // And then switch to waiting for input
+                    this.state = State.WAITINGINPUT;
+                    LOGGER.stateSet(state);
+                    break;
+                
+                case CREATINGCOMBAT:                    
+                    ArrayList<PlayerCharacter> players = new ArrayList<>(0);
+                    players.add(player);
+                    // For now we're gonna just use 1 random enemy. TODO improve
+                    this.currentFight 
+                            = new Combat(selectOpponents(enemyGetter, 1),
+                                        players);
+                    this.state = State.RUNNINGCOMBAT;
+                    LOGGER.stateSet(state);  
+                    break;
+                    
+                case RUNNINGCOMBAT:
+                    this.currentFight.update();
+                    break;
+                case WAITINGINPUT:
+                    if (userReady) {
+                        Board.this.state = State.CREATINGCOMBAT;
+                        LOGGER.stateSet(state);
+                    }
+                    break;
+            }
 
+            
         }
-    }    
-    
+    }
     
     @Override
     public void run(){
@@ -231,7 +293,7 @@ public class Board extends JPanel implements Runnable, Testing {
         // pin should be (i.e. what the image is in front of/behind, since 
         // recently-drawn elements overwrite less recently-drawn ones).
         try {
-        screenPins.add(pinCount, p);
+            screenPins.add(pinCount, p);
         }
         catch (IndexOutOfBoundsException e){
             LOGGER.log("Index out of bounds");
@@ -246,7 +308,8 @@ public class Board extends JPanel implements Runnable, Testing {
         }
     }
     
-    public ActionExtractor getActionGetter() throws JAXBException{
+// <editor-fold defaultstate="collapsed" desc=" Extractor getters for XML ">
+    public ActionExtractor getActionGetter() throws JAXBException {
         // Creates a new ActionExtractor object that contains all the Actions
         // in the XML actions file.
         ActionExtractor actionExtractor;
@@ -257,13 +320,16 @@ public class Board extends JPanel implements Runnable, Testing {
         actionExtractor = (ActionExtractor) unm.unmarshal(
                 cl.getResourceAsStream(ACTIONLOCATION));
         LOGGER.log("Found "
-                + actionExtractor.getListOfActions().size() 
+                + actionExtractor.getListOfActions().size()
                 + " actions.");
         
         return actionExtractor;
     }
     
-    public EnemyExtractor getEnemyGetter() throws JAXBException{
+    public EnemyExtractor getEnemyGetter() throws JAXBException {
+        // Creates a new EnemyExtractor object that contains all the Enemies
+        // in the XML file
+        // Is it unwise to load so much stuff at once? Probably.
         EnemyExtractor enemyExtractor;
         JAXBContext context = JAXBContext.newInstance(EnemyExtractor.class);        
         Unmarshaller unm = context.createUnmarshaller();        
@@ -272,12 +338,14 @@ public class Board extends JPanel implements Runnable, Testing {
         LOGGER.log("Extracting enemies from " + ENEMIESLOCATION);
         enemyExtractor = (EnemyExtractor) unm.unmarshal(
                 cl.getResourceAsStream(ENEMIESLOCATION));
-        LOGGER.log("Found " 
+        LOGGER.log("Found "
                 + enemyExtractor.getListOfEnemies().size()
                 + " enemies.");
         
         return enemyExtractor;
     }
+
+// </editor-fold>
     
     public Shape stringToShape(String str) {
         // Attempts at code to convert a string to a shape
@@ -296,6 +364,29 @@ public class Board extends JPanel implements Runnable, Testing {
         return null;
     }
     
+    private ArrayList<Enemy> selectOpponents(EnemyExtractor fetcher, int n){
+        // This method might later become part of the 'area' class, so
+        // each area can provide its own enemies
+        ArrayList<Enemy> shuffledEnemies = fetcher.getListOfEnemies();
+        Collections.shuffle(shuffledEnemies);
+        ArrayList<Enemy> output 
+                = new ArrayList<>(shuffledEnemies.subList(0, n));
+        return output;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent acEve) {
+            if ("startCombat".equals(acEve.getActionCommand())){
+                LOGGER.log("Button pressed");
+                userReady = true;                
+            }
+    }
+    
+    /**
+     * Inner class: combat. Better to begin this chain inside the board,
+     * than mess around with passing board to round as an argument (as it did
+     * previously)
+     */    
     private class Combat {
         // Combat should probably be a subclass of Board, since it's going to be 
         // running other stuff essentially.
@@ -322,10 +413,11 @@ public class Board extends JPanel implements Runnable, Testing {
             this.playerList = players;
             this.enemyList  = enemies;
         }
-        
+               
         public void update(){
             if (this.currentRound == null) {
                 // No round exists yet
+                // TODO add a pause between rounds
                 LOGGER.log("Null Round detected, creating new Round");
                 // or something else the actbar object can self-alter
                 
@@ -337,7 +429,8 @@ public class Board extends JPanel implements Runnable, Testing {
                 // Add all the pins in the round to the board
 
                 addScreenObject(currentRound.pins.toArray(new Pin[0]));                
-                LOGGER.log("Adding " + currentRound.pins.toArray(new Pin[0]).length 
+                LOGGER.log("Adding " 
+                        + currentRound.pins.toArray(new Pin[0]).length 
                         + " screen objects; total screen objects: " 
                         + pinCount);                
             }            
@@ -353,7 +446,11 @@ public class Board extends JPanel implements Runnable, Testing {
         
     }
     
-    private class MyAdapter extends KeyAdapter{
+    /**
+     * Inner class: switchboard for input events. Cast to KeyListener or
+     * ActionListener as appropriate. Is that bad practice? Probably
+     */
+    private class MyKeyAdapter extends KeyAdapter {
         
         @Override
         public void keyReleased(KeyEvent release){
@@ -380,5 +477,19 @@ public class Board extends JPanel implements Runnable, Testing {
                     break;
             }
         }
-    }    
+
+        }
+
+    
+//    private class MyActionAdapter implements ActionListener {
+//
+//        @Override
+//        public void actionPerformed(ActionEvent e){
+//            if ("combatStarted".equals(e.getActionCommand())){
+//                LOGGER.log("Button pressed");
+//                userReady = true;
+//            }        
+//        }
+//    }  
+
 }
